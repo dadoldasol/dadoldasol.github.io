eb69c0f4de9d11747ca028bf18fa9e6a986322a1
Dasol <dasol8297@gmail.com>
2022-05-10 23:06:30 +0900

    add cleancode category and delete dip

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,9 +5,15 @@ var store = [{
         "url": "/algo/leetcode-week1/",
         "teaser": null
       },{
-        "title": "DIP Test",
-        "excerpt":"dip test  ","categories": ["dip"],
+        "title": "알고리즘 문제 풀이 기록",
+        "excerpt":"여러 가지 이유로 12월 중순부터 알고리즘 문제를 풀기 시작했다. C++ STL을 제대로 모른다. (회사에서는 거의 몰라도 되는 수준의 코딩을 진행함) 이직을 위한 준비 알고리즘 실력을 향상시키고자 하는 마음 처음에는 이직을 위한 원대한 꿈을 가지고 시작했지만… 코딩한다는 사람 치고 너무 모르는 것 같아서 이직 생각은 반쯤 버리고 시간 나는 대로 풀고...","categories": ["algo"],
         "tags": [],
-        "url": "/dip/dip-test/",
+        "url": "/algo/algo-first/",
+        "teaser": null
+      },{
+        "title": "Ch1. 깨끗한 코드",
+        "excerpt":"코드는 존재하리라 궁극적으로 코드는 요구사항을 표현하는 언어라는 사실을 명심한다. 요구사항에 더욱 가까운 언어를 만들 수도 있고, 요구사항에서 정형 구조를 뽑아내는 도구를 만들 수도 있다. 하지만 어느 순간에는 정밀한 표현이 필요하다. 그 필요성을 없앨 방법은 없다. 그러므로 코드는 항상 존재하리라. 나쁜 코드 르블랑의 법칙(leblanc’s Law) : 나중은 결코 오지 않는다. 나쁜...","categories": ["cleancode"],
+        "tags": [],
+        "url": "/cleancode/cleancode-ch1/",
         "teaser": null
       }]
